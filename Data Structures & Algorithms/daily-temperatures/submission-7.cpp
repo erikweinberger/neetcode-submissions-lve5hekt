c6ef9039ba1a39b041af7617c54f5b5f9da4872c
Erik Per Wilhelm Weinberger <139198715+erikweinberger@users.noreply.github.com>
2026-04-05 15:42:46 +1000

    Add: daily-temperatures - submission-7

---
+++ Data Structures & Algorithms/daily-temperatures/submission-7.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        auto res = vector(n, 0);
+
+        vector<int> stk;
+        stk.reserve(n);
+
+        for (int i{n - 1}; i >= 0; --i) {
+
+            while (!stk.empty() && temperatures[stk.back()] <= temperatures[i]) {
+                stk.pop_back();
+            }
+
+            res[i] = stk.empty() ? 0 : stk.back() - i;
+
+            stk.push_back({i});
+        }
+
+        return res;
+    }
+};
